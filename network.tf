b7466ea7f7b7744fa4c48734a1797522918dae0c
josielsouzanordcloud <90701149+josielsouzanordcloud@users.noreply.github.com>
2022-12-13 14:38:57 +0000

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/josiel-nc/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "network"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
